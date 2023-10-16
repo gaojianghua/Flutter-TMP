d7d2a75aed8ac4c89f2a6bfbfed6c497e606683b
高江华 <g598670138@163.com>
2023-10-16 12:42:08 +0800

    更新

--- lib/store/cart_store.dart
+++ lib/store/cart_store.dart
@@ -2,7 +2,7 @@
  * @Author: 高江华 g598670138@163.com
  * @Date: 2023-10-12 15:48:27
  * @LastEditors: 高江华
- * @LastEditTime: 2023-10-16 11:55:18
+ * @LastEditTime: 2023-10-16 12:41:58
  * @Description: file content
  */
 import 'package:flutter_bloc/flutter_bloc.dart';
