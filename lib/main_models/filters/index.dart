89b63beece5111b97a02dcbee77bd4e0d6461730
高江华 <g598670138@163.com>
2024-02-04 14:51:43 +0800

    加入自定义组件文件

---
+++ lib/main_models/filters/index.dart
@@ -0,0 +1,12 @@
+/*
+ * @Author: 高江华 g598670138@163.com
+ * @Date: 2024-02-04 14:44:32
+ * @LastEditors: 高江华
+ * @LastEditTime: 2024-02-04 14:44:46
+ * @Description: file content
+ */
+
+
+class Filters {
+  
+}
---
+++ lib/main_models/main_ui/m-agree-policy/index.dart
@@ -0,0 +1,8 @@
+/*
+ * @Author: 高江华 g598670138@163.com
+ * @Date: 2024-02-04 14:50:35
+ * @LastEditors: 高江华
+ * @LastEditTime: 2024-02-04 14:50:40
+ * @Description: file content
+ */
+
